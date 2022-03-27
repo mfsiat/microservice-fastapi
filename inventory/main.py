bd5f4acc8bf82499580476b78aea96c816007a9f
Md Nasirul Islam <nasirul.siat@outlook.com>
2022-03-28 02:25:20 +0600

    implemented redis configs

--- inventory/main.py
+++ inventory/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from redis_om import get_redis_connection
 
 app = FastAPI()
+
+# init redis
+redis = get_redis_connection(
+  host="redis-13515.c252.ap-southeast-1-1.ec2.cloud.redislabs.com",
+  port=13515,
+  password="H21OzG3kvmL2kyewXqOBABtsITUcunMl",
+  decode_responses=True
+)
 
 @app.get("/")
 async def root():
